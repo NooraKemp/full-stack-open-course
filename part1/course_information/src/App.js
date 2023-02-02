1b628a52c6defc9a188282dd9fe70f1d061a02e0
Noora Kemppainen <noora.kemppainen@helsinki.fi>
2023-02-02 07:26:01 +0200

    Update tasks

--- part1/course_information/src/App.js
+++ part1/course_information/src/App.js
@@ -24,9 +24,9 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises:{" "} 
-      {props.parts[0].exercises + 
-      props.parts[1].exercises + 
-      props.parts[2].exercises}
+      {props.parts[0].exercises 
+      + props.parts[1].exercises 
+      + props.parts[2].exercises}
     </p>
   )
 }
